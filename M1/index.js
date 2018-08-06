63094468e863aa72eaae9cbf5d290cd8469933d2
Alberto Lacerda <albertoflfilho@gmail.com>
2018-08-06 09:33:31 -0400

    get /test

--- M1/index.js
+++ M1/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('Node.js');
 })
 
 app.listen(3000);
-//localhost:3000
