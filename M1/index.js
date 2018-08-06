80763036df88f51b617bb38542aaf2a8a10007c0
Alberto Lacerda <albertoflfilho@gmail.com>
2018-08-06 12:53:02 -0400

    npm install nunjucks

--- M1/index.js
+++ M1/index.js
@@ -1,17 +1,9 @@
 const express = require('express');
+
 const app = express();
 
-app.use('/user', (req, res, next) => {
-  console.log('Accessed...');
-  next();
-});
-
-app.get('/user', (req, res) => {
-  res.send("User: albertoflfilho");
-});
-
-app.get('/user/name', (req, res) =>{
-  res.send('Name: Alberto Lacerda');
+app.get('/', (req, res) => {
+  
 });
 
 app.listen(3000);
