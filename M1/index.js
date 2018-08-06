e4ce16ee83deac5194d3037888684b2879fd47fd
Alberto Lacerda <albertoflfilho@gmail.com>
2018-08-06 12:33:57 -0400

    console.log('Accessed...') by const userMiddleware only

--- M1/index.js
+++ M1/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+const userMiddleware = (req, res, next) => {
   console.log('Accessed...');
   next();
-})
+};
 
 
 app.get('/test', (req, res) => {
   res.send("Hey Express!");
 })
 
-app.get('/user', (req, res) =>{
+app.get('/user', userMiddleare, (req, res) =>{
   res.send('User: Alberto');
 })
 
