485e53654807ed096b4993fd482374cde81eb289
Alberto Lacerda <albertoflfilho@gmail.com>
2018-08-06 17:36:04 -0400

    app.get render array view njk

--- M1/index.js
+++ M1/index.js
@@ -13,7 +13,7 @@ app.set('view engine', 'njk');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.render('index', { name: 'Alberto Lacerda'});
+  res.render('index', { names: ['Alberto Lacerda', 'Node.js'] });
 });
 
 app.listen(3000);
