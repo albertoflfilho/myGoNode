d6f24fac6466854a512998eafa627627a48d1555
Alberto Lacerda <albertoflfilho@gmail.com>
2018-08-07 11:26:00 -0400

    configuring and npm install eslint eslint-plugin-import eslint-config-airbnb-base -D

--- M1/index.js
+++ M1/index.js
@@ -7,24 +7,21 @@ const app = express();
 
 nunjucks.configure('views', {
   autoescape: true,
-  express: app
+  express: app,
 });
 
 app.set('view engine', 'njk');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.post('/auth', (req, res) => {
-
   const { username, password } = req.body;
-
-  res.send(`We can find the current logged in username ${ username } and password ${ password }`);
-
-})
+  res.send(`We can find the current logged in username ${username} and password ${password}`);
+});
 
 app.listen(3000);
